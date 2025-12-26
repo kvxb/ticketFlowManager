58555c2b8d2709d1c2e3fc9ddd2be9528f8cff12
kvxb <justin.raindust@gmail.com>
2025-12-26 23:42:27 +0200

    structure project in OOP manner by decoupling App

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import java.time.LocalDate;
+import tickets.Ticket;
 
 /*
 https://www.baeldung.com/jackson-annotations
@@ -28,6 +29,7 @@ public class App {
     }
 
     private static final String INPUT_USERS_FIELD = "input/database/users.json";
+    private static List<Ticket> tickets;
     private static LocalDate second;
     private static List<CommandInput> commands;
 
@@ -39,6 +41,11 @@ public class App {
      * @param outputPath path to the file where results should be written
      */
 
+    // think how this method should work it should be as decoupled from the logic of
+    // the commands as that should be implmeneted in their respective classes
+    // but then who holds the commands for example ? this class should right ?
+    // unless you can make them float as a sort of static class ? think about it
+    // tomorrow i cba today
     public static void testingPeriod() {
         LocalDate initialDate = second;
         LocalDate finishDate = second.plusDays(12);
@@ -47,13 +54,17 @@ public class App {
         int it = 0;
 
         while (!time.isAfter(finishDate)) {
-            time = commands.get(it).time();
             /*
-             *give every ticket an id (keep a static variable to keep count of them (first check what rule they follow for example what if another testingperiod starts))
-             *verify if the data is fine (only bug tickets can be anonym or sum)
-             *create a list to hold the tickets
-             *create a method to viewTickets
-            */
+             * give every ticket an id (keep a static variable to keep count of them (first
+             * check what rule they follow for example what if another testingperiod
+             * starts))
+             * verify if the data is fine (only bug tickets can be anonym or sum)
+             * create a list to hold the tickets
+             * create a method to viewTickets
+             */
+            CommandInput currentCommand = commands.get(it);
+            time = currentCommand.time();
+
             it += 1;
         }
     }
@@ -90,9 +101,22 @@ public class App {
         // for(var second =
         // commands.getFirst().time();!second.isAfter(commands.getLast().time()); second
         // = second.plusDays(1)) {
-        testingPeriod();
-        developPeriod();
-        verifiyPeriod();
+        //
+        //
+        //
+        //
+        // either do a while loop through these 3 stages until the thing that stops them
+        // happens
+        //
+        // or make it so that these are the minimal stages and verify can call
+        // testingPeriod in the code of it which makes sense code wise as i understand
+        // it but might not be optimal for readability reasons
+        boolean LOOPBACK = true;
+        while (LOOPBACK) {
+            testingPeriod();
+            developPeriod();
+            verifiyPeriod();
+        }
         // }
 
         // TODO 3: create objectnodes for output, add them to outputs list.
